b6e41d1864afd69b7ca7eb259c2986e428b51f70
Grafe <flingelfrank@hotmail.com>
2013-06-03 10:34:31 +0200

    Initial commit
    Signed-off-by: Grafe <flingelfrank@hotmail.com>

---
+++ src/com/dre/chunkkeeper/P.java
@@ -0,0 +1,17 @@
+package com.dre.chunkkeeper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin{
+	public static P p;
+	
+	@Override
+	public void onEnable(){
+		p = this;
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
